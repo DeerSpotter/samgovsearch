1b9b2dfd47b48277dd975cee9cf3ca9126c27236
DeerSpotter <m.teleguz1@gmail.com>
2026-06-24 14:54:33 -0400

    Add initial match app wrapper

---
+++ samgovsearch_pro_initial_match.py
@@ -0,0 +1,14 @@
+from samgovsearch_pro_download_folder import SamGovSearchProDownloadFolderApp
+
+
+class SamGovSearchProInitialMatchApp(SamGovSearchProDownloadFolderApp):
+    pass
+
+
+def main():
+    app = SamGovSearchProInitialMatchApp()
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
